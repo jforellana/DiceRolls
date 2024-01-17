40312c137029bd99b50868130643866a09d7c97d
jforellana <mrjfob@byu.edu>
2024-01-17 11:47:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using dice;
+using System.Collections;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Xml;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        diceroll dr;
+
+        dr = new diceroll();
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n\n" +
+            "How many dice rolls would you like to simulate?");
+
+        int dice = int.Parse(System.Console.ReadLine());
+
+        int[] results = dr.rolldie(dice);
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            float percent = (float)results[i]/(float)dice;
+            int round = (int)(percent*100);
+
+            string output = "";
+            output = (i+2)+":\t";
+
+            for (int j = 0; j < round; j++)
+            {
+                output += "*";
+            }
+            System.Console.WriteLine(output);
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ diceroll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice
+{
+
+    internal class diceroll
+    {
+        private int die1;
+        private int die2;
+        private int diesum;
+        public int[] rolldie(int rolls)
+        {
+            Random random = new Random();
+
+            int[] results = new int[11];
+
+            for (int i = 0; i < rolls; i++)
+            {
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                diesum = die1 + die2;
+                results[diesum-2] = results[diesum-2] + 1;
+            }
+
+            return results;
+        }
+
+    }
+}
