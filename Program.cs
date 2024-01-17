f8a3bc08175ef027cdfb17cdcaafe302d60b476e
jforellana <mrjfob@byu.edu>
2024-01-17 12:09:36 -0700

    add comments

--- Program.cs
+++ Program.cs
@@ -16,22 +16,30 @@ internal class Program
         System.Console.WriteLine("Welcome to the dice throwing simulator!\n\n" +
             "How many dice rolls would you like to simulate?");
 
+        // save user input
         int dice = int.Parse(System.Console.ReadLine());
 
+        // send die to rolldie class and store the results in results array
         int[] results = dr.rolldie(dice);
 
-        for (int i = 0; i < results.Length; i++)
+        // output the results
+        for (int i = 0; i < results.Length; i++) 
         {
+            // convert the number of times each number was rolled into a decimal
             float percent = (float)results[i]/(float)dice;
+            // convert float to integer
             int round = (int)(percent*100);
 
             string output = "";
             output = (i+2)+":\t";
 
+            // create a string of asteriscs for each possible rolldie total
             for (int j = 0; j < round; j++)
             {
                 output += "*";
             }
+
+            // output results in console
             System.Console.WriteLine(output);
         }
 
